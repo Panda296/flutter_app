9161a41313c25ef1562f741877b6e0c61dc947d2
jiangling <296131948@qq.com>
2021-01-15 06:45:14 +0800

    Hello World 及 Text 控件的基本应用

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,19 @@
 import 'package:flutter/material.dart';
 
+/**
+ * 1 创建 main 函数
+ * 2 执行 runApp 函数
+ * 3 传入 Widget -> Text Widget
+ *
+ * 问题:  文字没有居中
+ *        文字太小
+ */
+
+/// main 函数
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
+  runApp(Text(
+    "Hello World",
+    //Text 需要传入一个方向参数
+    textDirection: TextDirection.ltr,
+  ));
 }
