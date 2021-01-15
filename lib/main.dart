eb9cc2c671f8700d5e89e0f0aa29fe2d5da7380a
panda <296131948@qq.com>
2021-01-15 20:56:46 +0800

    简单的实例 *   Colum 列表控件,用于展示列表           但是所展示的控件内容不能超过屏幕,否则报错 *   ListView 超出屏幕内容默认滚动 *   SizedBox 提供间距 *   Padding 添加边距 *   添加边框 -> Container 空间中的decoration 属性

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,69 @@
 import 'package:flutter/material.dart';
 
-/**
- * 1 创建 main 函数
- * 2 执行 runApp 函数
- * 3 传入 Widget -> Text Widget
- *
- * 问题:  文字没有居中
- *        文字太小
- */
-
-/// main 函数
-void main() => MyApp();
+main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Hello World",
       home: Scaffold(
-          appBar: AppBar(
-            title: Center(child: Text("Title")),
+        appBar: AppBar(
+          title: Text("Panda"),
+        ),
+        body: Homecontent(),
+      ),
+    );
+  }
+}
+
+class Homecontent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView(
+          children: [
+            ProductItem(
+                "黑寡妇", "不一样的寡姐", "http://39.105.189.216/html/player/img/1.png"),
+            ProductItem("绿巨人", "绿巨人的成长之路",
+                "http://39.105.189.216/html/player/img/2.png"),
+            ProductItem("美国队长", "美国队长的爱恨情仇",
+                "http://39.105.189.216/html/player/img/3.png"),
+          ],
+        ));
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  final String imgUrl;
+
+  ProductItem(this.title, this.subTitle, this.imgUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(border: Border.all(width: 1)),
+      padding: EdgeInsets.all(4),
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Text(
+            title,
+            style: TextStyle(fontSize: 20),
           ),
-          body: ContentWidget()),
-    );
-  }
-}
-
-class ContentWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: TextWidget(),
-    );
-  }
-}
-
-class TextWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "Hello World",
-      //Text 需要传入一个方向参数
-      textDirection: TextDirection.ltr,
-      style: TextStyle(color: Colors.yellow, fontSize: 40),
+          SizedBox(height: 8),
+          Text(
+            subTitle,
+            style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(height: 8),
+          Image.network(imgUrl)
+        ],
+      ),
     );
   }
 }
