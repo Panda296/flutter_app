115ca56a8a2517a2017912b7a30a61ce38bb799c
panda <296131948@qq.com>
2021-01-16 11:57:02 +0800

    base 代码提交,用于模板

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
-}
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "StatefulWidget",
       home: Scaffold(
         appBar: AppBar(
           title: Text("Panda"),
@@ -21,49 +20,15 @@ class MyApp extends StatelessWidget {
 class Homecontent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView(
-          children: [
-            ProductItem(
-                "黑寡妇", "不一样的寡姐", "http://39.105.189.216/html/player/img/1.png"),
-            ProductItem("绿巨人", "绿巨人的成长之路",
-                "http://39.105.189.216/html/player/img/2.png"),
-            ProductItem("美国队长", "爱恨情仇英雄路",
-                "http://39.105.189.216/html/player/img/3.png"),
-          ],
-        ));
+    return Center(
+      child: TextWidget(),
+    );
   }
 }
 
-class ProductItem extends StatelessWidget {
-  final String title;
-  final String subTitle;
-  final String imgUrl;
-
-  ProductItem(this.title, this.subTitle, this.imgUrl);
-
+class TextWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(border: Border.all(width: 1)),
-      padding: EdgeInsets.all(4),
-      child: Column(
-        children: [
-          SizedBox(height: 20),
-          Text(
-            title,
-            style: TextStyle(fontSize: 20),
-          ),
-          SizedBox(height: 8),
-          Text(
-            subTitle,
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 8),
-          Image.network(imgUrl)
-        ],
-      ),
-    );
+    return Text("StatefulWidget");
   }
 }
