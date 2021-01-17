28a8b322c6e62cfd04f6d780ede72e75ff0ee59d
panda <296131948@qq.com>
2021-01-17 14:39:52 +0800

    TextField 的使用及常见属性

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,56 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Panda"),
         ),
-        body: Homecontent(),
+        body: Register(),
       ),
     );
   }
 }
 
-class Homecontent extends StatelessWidget {
+class Register extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: TextWidget(),
-    );
+  State<StatefulWidget> createState() {
+    return RegisterState();
   }
 }
 
-class TextWidget extends StatelessWidget {
+class RegisterState extends State {
+  final controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.text = "默认值";
+    controller.addListener(() {
+      print("controller 监听到变化  ${controller.value}");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("StatefulWidget");
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.person),
+              labelText: "请输入您的账号",
+              labelStyle: TextStyle(fontSize: 20, color: Colors.lightGreen),
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 1),
+                  borderRadius: BorderRadius.circular(8)),
+              hintText: "username",
+              // filled: true, // 是否填充背景
+              // fillColor: Colors.orange, // 填充背景色
+            ),
+            onChanged: (value) => print("当前输入的值 $value"),
+            onSubmitted: (value) => print("当前提交的值 $value"),
+            controller: controller,
+          )
+        ],
+      ),
+    );
   }
 }
